560000d1d55536274071bb927086cc66a5f352c9
Admin_amolin19 <Admin_amolin19@its.jnj.com>
2022-06-08 14:31:10 -0400

    initial commit for ASSURE DbProfileModule

---
+++ SettingsFunctions.R
@@ -0,0 +1,14 @@
+createDbProfileModuleSpecifications <- function() 
+{
+
+  checkmate::assert_file_exists("MetaData.json")
+  moduleInfo <- ParallelLogger::loadSettingsFromJson("MetaData.json")
+  
+  specifications <- list(module = moduleInfo$Name,
+                         version = moduleInfo$Version,
+                         remoteRepo = "github.com",
+                         remoteUsername = "ohdsi")
+						 
+  class(specifications) <- c("dbProfileModuleSpecifications", "ModuleSpecifications")
+  return(specifications)
+}
